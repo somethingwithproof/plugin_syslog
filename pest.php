2db1b7fc8405ffff0db6bb712a68aee2820170f6
TheWitness <thewitness@cacti.net>
2026-04-09 20:45:28 -0400

    fix: Removing files in place that are not applicable

--- pest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-
-uses()->group('unit')->in('tests/Unit');
-uses()->group('integration')->in('tests/Integration');
-uses()->group('security')->in('tests/Security');
